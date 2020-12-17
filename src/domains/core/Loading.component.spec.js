c27f70d6c2ee66a328f1add496fbd3761dc8d822
tonymtz <atony.mtz@gmail.com>
2020-12-16 20:20:30 -0700

    [SB-28] Make eslint pass part of PR validation step

---
+++ src/domains/core/Loading.component.spec.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import LoadingComponent from './Loading.component';
+
+test('it renders', () => {
+  render(<LoadingComponent />);
+});
