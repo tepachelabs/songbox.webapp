fca60232d5e72e2f4ebe69b73456f27d15365a48
imariel2d <imariel2d@gmail.com>
2020-12-21 11:33:24 -0700

    [SB-30] Play song fetched from dropbox

--- src/components/AudioPlayer/Audio/Audio.container.js
+++ src/components/AudioPlayer/Audio/Audio.container.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import AudioComponent from './Audio.component';
@@ -6,11 +6,22 @@ import AudioComponent from './Audio.component';
 const AudioContainer = () => {
   const autoPlay = useSelector((state) => state.settings.get('autoPlay'));
   const songLink = useSelector((state) => state.player.get('songLink'));
+  const audioRef = React.createRef();
+
+  useEffect(() => {
+    if (audioRef.current && songLink) {
+      audioRef.current.load();
+      audioRef.current.play()
+        .then(() => console.log('play')) // Change console log for something useful
+        .catch(() => console.log('error')); // Change console log for something useful
+    }
+  }, [audioRef, songLink]);
 
   return (
     <AudioComponent
       autoPlay={autoPlay}
       songLink={songLink}
+      audioRef={audioRef}
     />
   );
 };
