ab8144ed2f72180feabac3daf432d0aa3d13c2bd
Ariel Plascencia <imariel2d@gmail.com>
2020-12-22 19:57:07 -0700

    [SB-25] streamable song link (#9)
    * [SB-25] Fetch streamable song
    
    * [SB-25] Fetch streamable song
    
    * [SB-25] streamable song link
    
    * [SB-25] Fetch streamable song

--- src/store/actions/playerActions.js
+++ src/store/actions/playerActions.js
@@ -1,3 +1,4 @@
+import { apiFetchStreamableSong } from 'lib/apiFetchStreamableSong';
 import {
   PLAYER_SET_REPEAT,
   PLAYER_SET_VOLUME,
@@ -9,3 +10,16 @@ export const toggleRepeat = (payload) => ({ type: PLAYER_SET_REPEAT, payload });
 export const setVolume = (payload) => ({ type: PLAYER_SET_VOLUME, payload });
 export const setSong = (payload) => ({ type: PLAYER_SET_CURRENT_SONG, payload });
 export const setSongLink = (payload) => ({ type: PLAYER_SET_SONG_LINK, payload });
+
+export const getSongStreamLink = (path) => (dispatch, getState) => {
+  const token = getState().app.get('token');
+
+  apiFetchStreamableSong(token, path)
+    .then(({ data }) => {
+      const streamableSong = data.url.replace('?dl=0', '?dl=1');
+      dispatch(setSongLink(streamableSong));
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+};
