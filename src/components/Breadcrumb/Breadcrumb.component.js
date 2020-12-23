024a89259c900a985dc2242fcca9b74bc8e3ce27
imariel2d <imariel2d@gmail.com>
2020-12-23 12:16:10 -0700

    [SB-14] Webapp > Breadcrumb

--- src/components/Breadcrumb/Breadcrumb.component.js
+++ src/components/Breadcrumb/Breadcrumb.component.js
@@ -1,42 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
 import propTypes from 'prop-types';
+
 import Home from '../Home';
 
 import './style/breadcrumb.scss';
 
-const BreadcrumbComponent = ({ clickableFolders, showingRoute }) => {
-  const darkThemeActive = useSelector((state) => state.player.darkTheme);
-
-  return (
-    <div className="directory-container">
-      <div className="sub-directory-container">
-        <Home />
-        <div className={`route-refresh-container ${darkThemeActive ? 'dark-theme-background' : ''}`}>
-          <h4 className="title">
-            {
-              clickableFolders.length >= 1
-                ? clickableFolders.map((tempRoute, index) => (
-                  <Link className={`${darkThemeActive ? 'dark-theme-color' : ''} route-breadcrumb`} key={tempRoute} to={`/app${tempRoute}`}>
-                    {`/${showingRoute[index]}`}
-                  </Link>
-                ))
-                : (
-                  '/'
-                )
-            }
-          </h4>
-        </div>
-      </div>
+const BreadcrumbComponent = ({ breadcrumbs }) => (
+  <div className="directory-container">
+    <Home />
+    <div className="breadcrumb">
+      {
+        breadcrumbs.map(({ title, uri }) => (title ? (
+          <Link key={title} className="route-breadcrumb" to={`${uri}`}>
+            {`/${title}`}
+          </Link>
+        ) : undefined))
+      }
     </div>
-  );
-};
+  </div>
+);
 
 BreadcrumbComponent.propTypes = {
-  clickableFolders: propTypes.arrayOf(propTypes.string).isRequired,
-  showingRoute: propTypes.arrayOf(propTypes.string).isRequired,
+  breadcrumbs: propTypes.arrayOf(Object).isRequired,
 };
 
 export default BreadcrumbComponent;
--- src/components/Breadcrumb/Breadcrumb.container.js
+++ src/components/Breadcrumb/Breadcrumb.container.js
@@ -1,44 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 import BreadcrumbComponent from './Breadcrumb.component';
 
 const BreadcrumbContainer = () => {
-  const { path } = useParams();
+  const location = useLocation();
 
-  const [showingRoute, setShowingRoute] = useState([]);
-  const [clickableFolders, setClickableFolders] = useState([]);
-
-  useEffect(() => {
-    const myRoute = path || '';
-    const cleanRoute = myRoute.replaceAll('%20', ' ');
-
-    const splitBreadCrumb = cleanRoute.split('/');
-    const cleanRoutes = splitBreadCrumb.filter((tempRoute) => tempRoute !== '');
-    const finalRoutes = [];
-
-    for (let i = 0; i < cleanRoutes.length; i += 1) {
-      let tempRoute = '';
-      for (let j = 0; j <= i; j += 1) {
-        tempRoute = `${tempRoute}/${cleanRoutes[j]}`;
-      }
-
-      finalRoutes.push(`${tempRoute}/`);
-    }
-
-    setClickableFolders(finalRoutes);
-    setShowingRoute(cleanRoutes);
-  }, [path]);
+  const split = location.pathname.split('/');
+  const breadcrumbs = split.reduce((paths, current) => {
+    const uri = paths.map((path) => path.title);
+    return [
+      ...paths,
+      {
+        title: current,
+        uri: `${uri.join('/')}/${current}`,
+      },
+    ];
+  }, []);
 
   return (
     <BreadcrumbComponent
-      clickableFolders={clickableFolders}
-      showingRoute={showingRoute}
+      breadcrumbs={breadcrumbs}
     />
   );
-};
-
-BreadcrumbContainer.propTypes = {
 };
 
 export default BreadcrumbContainer;
