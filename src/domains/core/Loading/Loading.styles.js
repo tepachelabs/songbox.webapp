4c56096755f3049911558252c67873494e840a81
imariel2d <imariel2d@gmail.com>
2021-02-02 08:51:15 -0700

    Replacing SCSS for styled components

---
+++ src/domains/core/Loading/Loading.styles.js
@@ -0,0 +1,28 @@
+import styled, { keyframes } from 'styled-components';
+
+const spinAnimation = keyframes`
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const LoadingBox = styled.div`
+  align-items: center;
+  display: flex;
+
+  &::after {
+    animation: ${spinAnimation} 0.7s infinite;
+    border: 5px solid transparent;
+    border-top-color: #FFA834;
+    border-radius: 50%;
+    content: "";
+    height: 60px;
+    left: 50%;
+    position: absolute;
+    top: 50%;
+    width: 60px;
+  }
+`;
