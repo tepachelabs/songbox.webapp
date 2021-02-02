ed58a5280ad07f37d14a18ad8879cbee2c92804f
Ariel Plascencia <imariel2d@gmail.com>
2021-02-02 08:14:43 -0700

    Adding tests (#38)

---
+++ src/components/HeartFavorite/HearthFavorite.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+
+import HeartFavorite from './HeartFavorite';
+
+describe('HearthFavorite component', () => {
+  test('it should render', () => {
+    const mockFn = jest.fn();
+    const isFavorite = false;
+
+    render(
+      <HeartFavorite
+        onClick={mockFn}
+        isFavorite={isFavorite}
+      />,
+    );
+  });
+
+  test('it should render and be clicked', () => {
+    const mockFn = jest.fn();
+    const isFavorite = true;
+
+    const component = render(
+      <HeartFavorite
+        onClick={mockFn}
+        isFavorite={isFavorite}
+      />,
+    );
+
+    const { firstChild } = component.container;
+    fireEvent.click(firstChild);
+
+    expect(mockFn).toHaveBeenCalled();
+  });
+
+  test('it should render and should not be clicked', () => {
+    const mockFn = jest.fn();
+    const isFavorite = true;
+
+    render(
+      <HeartFavorite
+        onClick={mockFn}
+        isFavorite={isFavorite}
+      />,
+    );
+
+    expect(mockFn).not.toHaveBeenCalled();
+  });
+});
