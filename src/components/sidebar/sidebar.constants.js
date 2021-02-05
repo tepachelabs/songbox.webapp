db1c22bbf469c028e9d683e399b71f98d9d1d871
imariel2d <imariel2d@gmail.com>
2021-02-05 10:19:17 -0700

    Capitalize F for Favorites in sidebar

--- src/components/sidebar/sidebar.constants.js
+++ src/components/sidebar/sidebar.constants.js
@@ -16,7 +16,7 @@ import {
 
 export const MENU_ITEMS = [
   { icon: HomeIcon, title: 'Home', link: APP_PATH },
-  { icon: HeartIcon, title: 'favorites', link: FAVORITES_PATH },
+  { icon: HeartIcon, title: 'Favorites', link: FAVORITES_PATH },
   { icon: SettingsIcon, title: 'Settings', link: SETTINGS_PATH },
   { icon: HelpCircleIcon, title: 'Help', link: HELP_PATH },
   { icon: LogOutIcon, title: 'Sign out', link: LOGIN_PATH },
