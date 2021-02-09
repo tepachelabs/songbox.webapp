40b759a33fc9751cf07660c2e64ad52b6e3962bd
tonymtz <atony.mtz@gmail.com>
2021-02-09 00:03:56 -0700

    fixes eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production';
+const warnDevErrorProd = isProd ? 2 : 1;
+
 module.exports = {
   env: {
     browser: true,
     es6: true,
-    jest: true
+    jest: true,
   },
   extends: [
     'plugin:react/recommended',
@@ -22,21 +25,23 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: [
-    'react', 'react-hooks'
+    'react', 'react-hooks',
   ],
   rules: {
-    "indent": ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    "import/prefer-default-export": "off",
-    "react/jsx-filename-extension": "off",
-    "react/jsx-fragments": ["error", "element"],
-    "react/jsx-props-no-spreading": ["off"]
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'import/prefer-default-export': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-fragments': ['error', 'element'],
+    'react/jsx-props-no-spreading': ['off'],
+    'prettier/prettier': warnDevErrorProd,
+    'no-console': warnDevErrorProd,
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        paths: ["src"]
-      }
-    }
-  }
+        paths: ['src'],
+      },
+    },
+  },
 };
--- src/lib/errorLogger.js
+++ src/lib/errorLogger.js
@@ -3,14 +3,12 @@ import Rollbar from 'rollbar';
 import config from '../config';
 
 let isInitialized;
-// eslint-disable-next-line no-unused-vars
 let rollbar;
 
 export const initErrorLogger = () => {
   if (process.env.NODE_ENV === 'development') {
     console.info('Development mode');
   } else {
-    // eslint-disable-next-line no-unused-vars
     rollbar = new Rollbar({
       accessToken: config.rollbarKey,
       captureUncaught: true,
