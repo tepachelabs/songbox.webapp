5f3c301c9c7e89cce1afa380d82c8bd1e222fcc4
imariel2d <imariel2d@gmail.com>
2021-02-10 10:25:31 -0700

    lint fix

--- src/domains/audio-player/audio-player.component.js
+++ src/domains/audio-player/audio-player.component.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from 'react'
-import propTypes from 'prop-types'
+import React, { Fragment } from 'react';
+import propTypes from 'prop-types';
 
-import { Loading } from 'domains/core/Loading'
-import PlayerComponent from './player'
-import AudioProgressContainer from './audio-progress'
+import { Loading } from 'domains/core/Loading';
+import PlayerComponent from './player';
+import AudioProgressContainer from './audio-progress';
 
-import { AudioWrapper } from './audio-player.styles'
+import { AudioWrapper } from './audio-player.styles';
 
 const AudioPlayerComponent = ({
   audioRef,
@@ -15,21 +15,19 @@ const AudioPlayerComponent = ({
   isLoading,
 }) => (
   <AudioWrapper>
-    {
-      isLoading
-        ? ( <Loading/> )
-        : (
-        <Fragment>
-          <AudioProgressContainer
-            audioRef={audioRef}
-            updateCurrentTime={updateCurrentTime}
-          />
-          <PlayerComponent onClick={onClick} isDisabled={isDisabled}/>
-        </Fragment>
-      )
-    }
+    {isLoading ? (
+      <Loading />
+    ) : (
+      <Fragment>
+        <AudioProgressContainer
+          audioRef={audioRef}
+          updateCurrentTime={updateCurrentTime}
+        />
+        <PlayerComponent onClick={onClick} isDisabled={isDisabled} />
+      </Fragment>
+    )}
   </AudioWrapper>
-)
+);
 
 AudioPlayerComponent.propTypes = {
   audioRef: propTypes.shape({ current: propTypes.node }).isRequired,
@@ -37,6 +35,6 @@ AudioPlayerComponent.propTypes = {
   onClick: propTypes.func.isRequired,
   isDisabled: propTypes.bool.isRequired,
   isLoading: propTypes.bool.isRequired,
-}
+};
 
-export default AudioPlayerComponent
+export default AudioPlayerComponent;
