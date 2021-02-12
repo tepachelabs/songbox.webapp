01217f52cb47582b1eff1b213d8c66e0e28fb0a0
Rene Durazo <adurazo@nearsoft.com>
2021-02-11 19:33:56 -0700

    fix test coverage

--- src/domains/audio-player/audio-progress/audio-progress.test.js
+++ src/domains/audio-player/audio-progress/audio-progress.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
 import { render } from '@testing-library/react';
 
 import store from 'store';
@@ -9,10 +10,17 @@ import AudioProgress from 'domains/audio-player/audio-progress/index';
 test('it renders', () => {
   const ref = new Audio();
   const mockFn = jest.fn();
+  const mockTheme = {
+    spacing: {
+      one: 1
+    }
+  };
 
   render(
     <Provider store={store}>
-      <AudioProgress audioRef={ref} updateCurrentTime={mockFn} />
+      <ThemeProvider theme={mockTheme}>
+        <AudioProgress audioRef={ref} updateCurrentTime={mockFn} />
+      </ThemeProvider>
     </Provider>,
   );
 });
