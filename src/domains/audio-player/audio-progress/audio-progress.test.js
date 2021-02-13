cf3236b518fee9b80285f134c7ce778a8838e949
Rene Durazo <adurazo@nearsoft.com>
2021-02-13 13:22:47 -0700

    pr changes

--- src/domains/audio-player/audio-progress/audio-progress.test.js
+++ src/domains/audio-player/audio-progress/audio-progress.test.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from 'styled-components';
 import { render } from '@testing-library/react';
 
 import store from 'store';
 
 import AudioProgress from 'domains/audio-player/audio-progress/index';
+import StyleDecorator from '../../../stories/decorators/style-decorator';
 
 test('it renders', () => {
   const ref = new Audio();
   const mockFn = jest.fn();
-  const mockTheme = {
-    spacing: {
-      one: 1,
-    },
-  };
 
   render(
     <Provider store={store}>
-      <ThemeProvider theme={mockTheme}>
+      <StyleDecorator>
         <AudioProgress audioRef={ref} updateCurrentTime={mockFn} />
-      </ThemeProvider>
+      </StyleDecorator>
     </Provider>,
   );
 });
