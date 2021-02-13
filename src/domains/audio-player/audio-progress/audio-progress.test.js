5aae233e98b4a1a4fff2385e10a93c63f987d5ca
Rene Durazo <adurazo@nearsoft.com>
2021-02-13 16:27:43 -0700

    pr changes

--- src/domains/audio-player/audio-progress/audio-progress.test.js
+++ src/domains/audio-player/audio-progress/audio-progress.test.js
@@ -5,7 +5,7 @@ import { render } from '@testing-library/react';
 import store from 'store';
 
 import AudioProgress from 'domains/audio-player/audio-progress/index';
-import StyleDecorator from '../../../stories/decorators/style-decorator';
+import StyleDecorator from 'stories/decorators/style-decorator';
 
 test('it renders', () => {
   const ref = new Audio();
