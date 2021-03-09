dee280ec0e0e54a518d7756c79366bf34c19d26a
tonymtz <atony.mtz@gmail.com>
2021-03-08 18:47:12 -0700

    Sorts files and folders by name

--- src/store/reducers/files.js
+++ src/store/reducers/files.js
@@ -6,6 +6,8 @@ import {
   FILES_SET_IS_LOADING,
 } from '../constants';
 
+const byName = (a, b) => a.get('name').localeCompare(b.get('name'));
+
 const initialState = Map({
   folders: List(),
   files: List(),
@@ -15,9 +17,9 @@ const initialState = Map({
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case FILES_SET_FILES_LIST:
-      return state.set('files', payload);
+      return state.set('files', payload.sort(byName));
     case FILES_SET_FOLDER_LIST:
-      return state.set('folders', payload);
+      return state.set('folders', payload.sort(byName));
     case FILES_SET_IS_LOADING:
       return state.set('isLoading', payload);
     default:
